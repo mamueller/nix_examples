76b6703eb2b8c2a9d5ea704912d3aa25e61a88e3
Markus Müller <markus.mueller.1.g@googlemail.com>
2019-03-26 17:40:15 +0100

    not yet done with pill 14

--- pills/13/callPackageTest.nix
+++ pills/13/callPackageTest.nix
@@ -1,7 +1,11 @@
 let 
   add = {a ? 3, b } : a + b ;
   values = { a = 3; b = 5; c = 10; };
-  callPackageTest = set: f: f (builtins.intersectAttrs ( builtins.functionArgs f) set);
+  callPackageTest1 = set: f: f (builtins.intersectAttrs ( builtins.functionArgs f) set);
+  callPackageTest2 = set: f: overrides: f ((builtins.intersectAttrs ( builtins.functionArgs f) set) // overrides);
 in 
-  callPackageTest values add
+  { 
+    v1 = callPackageTest1 values add;
+    v2 = callPackageTest2 values add { b = 12; };
+  }
 
